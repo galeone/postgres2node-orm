be563e8c14dc0c8c4b9878fb03d502dd2e146bb4
Paolo Galeone <nessuno@nerdz.eu>
2013-11-01 00:01:06 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,72 @@
+postgres2node-orm
+============
+
+Description
+-------------
+
+Generate [node-orm2](https://github.com/dresende/node-orm2) models from a postgresql schema dump.
+
+To obtain a database schema dump use `pg_dump --schema-only`
+
+Usage
+-------
+
+```shell
+perl postgres2node-orm.pl databaseDefinition.sql outputdir
+```
+
+Where `databaseDefinition.sql` is the schema dump and `outputdir` is an existing directory.
+
+It generates one file per table, in the current format: `table_name.ts`
+
+The extension is for TypeScript file, but you can edit easily the script to generate .js files (and changing TypeScript closure `() => ` with  JavaScript closure`function ()`).
+
+Example
+----------
+
+In my dump I've got a table definiton like:
+
+```dump
+CREATE TABLE users (
+  id serial8 NOT NULL,
+  stamp timestamp(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  story json,
+  private boolean NOT NULL DEFAULT FALSE,
+  username varchar(90) NOT NULL,
+  password varchar(40) NOT NULL,
+  name varchar(60) NOT NULL,
+  surname varchar(60) NOT NULL,
+  email varchar(350) NOT NULL,
+  board_lang varchar(2) DEFAULT NULL,
+  timezone varchar(35) NOT NULL DEFAULT 'UTC',
+  PRIMARY KEY (counter),
+  CONSTRAINT usersLastCheck CHECK(EXTRACT(TIMEZONE FROM last) = '0') 
+);
+```
+
+The output in `outputdir/users.ts` is
+```TypeScript
+module.exports = (db, cb) => {
+    db.define("users",
+    /* definition */
+    {
+        id : { type: 'number', required: true, size: 8, rational: false},
+        stamp : { type: 'date', required: true, time: true , defaultValue: "NOW()"},
+        story : { type: 'object', required: false},
+        private : { type: 'boolean', required: true, defaultValue: "FALSE"},
+        username : { type: 'text', required: true, size: 90},
+        password : { type: 'text', required: true, size: 40},
+        name : { type: 'text', required: true, size: 60},
+        surname : { type: 'text', required: true, size: 60},
+        email : { type: 'text', required: true, size: 350},
+        board_lang : { type: 'text', required: false, size: 2, defaultValue: "NULL"},
+        timezone : { type: 'text', required: true, size: 35, defaultValue: "UTC"}    
+    },
+    /* options */
+    {
+        id: 'counter'
+    });
+    
+    return cb();
+};
+```
---
+++ postgres2node-orm.pl
@@ -0,0 +1,169 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+#main
+
+my ($filename, $outdir) = @ARGV;
+die("$0 databaseDefinition.sql outputdir\n") unless defined $filename || undef $outdir;
+die("outputdir must be a directory\n") unless -d $outdir;
+
+open my $input, '<', $filename or die("can't open $filename: $!\n");
+
+my $fullfile = "";
+while(<$input>) {
+    $fullfile .= $_;
+}
+
+print "Parsing $filename, extracting tables\n";
+
+while ( $fullfile =~ /CREATE TABLE ([a-z0-9_]+) \((.+?)\)\;/misg )
+{
+    our ($module, $enter, $key);
+	$enter = 0;
+    $key = "";
+    $module =
+<<HERE;
+module.exports = (db, cb) => {
+    db.define("$1",
+    /* definition */
+    {
+HERE
+    foreach( split(/\n/, $2, -1) ) {
+		if( (defined $_ ) and !($_ =~ /^$/)) {
+			my ($name, $type, $options) = get_options($_);
+            if($name =~ /primary|constraint/i) {
+                if($_ =~ /primary key\s*\(([a-z0-9_]+)\)/i ) {
+                    $key = $1;
+                }
+                next;
+            }
+			$module .= "        $name : " . &get_fields($type, $options) .",\n";
+			$enter = 1;
+		}
+    }
+	#remove ,\n
+	if($enter) {
+		$module = substr $module, 0, -2;
+	}
+    $module .=
+<<HERE;
+    
+    },
+    /* options */
+    {
+HERE
+    if($key ne "") {
+        $module .= "        id: '$key'";
+    }
+    $module .=
+<<HERE;
+
+    });
+    
+    return cb();
+};
+HERE
+
+    open my $output, '>', "$outdir/$1.ts" or die("Unable to create $outdir/$1.ts");
+    print $output $module;
+    close($output);
+}
+close($input);
+
+exit(0);
+
+sub get_fields {
+
+    my $type 	= do { @_ ? shift : 'error' };
+    my $options = do { @_ ? shift : 'error'};
+    
+    ($type eq 'error' || $options eq 'error') && die("wrong parameters");
+
+    our($_type, $type_size, $rational, $time, $defaultValue ) = 
+    (
+        #_type
+             $type =~ /(big|small)?int|integer|(big|small)?serial|int[2|4|8]|serial[2|4|8]+/i ? "integer" : #number
+             $type =~ /double precision|float[4|8]|real|decimal|numeric/i ? "float" : #number
+             $type =~ /(var)?char|text|character( varying)?|varbit/i ? "text" : #text
+             $type =~ /bool(ean)?/i ? "boolean" : #boolean
+             $type =~ /time(stamp)|date?/i  ? "date" : #date
+             $type =~ /json/i  ?  "object" : #object
+             $type =~ /bit( varying)?|varbit|interval|box|bytea|cidr|circle|inet|line|lseg|macaddr|money|path|point|polygon|tsquery|tsvector|txid_snapshot|uuid|xml/i ? "binary" :
+             "error",
+         #type_size
+             $type =~ /^(?:(?:var)?char|character(?: varying)?|int|serial)\s*\(?([0-9]+)\)?$/i ? $1 :
+             $type =~ /big(int|serial)/i  ? '8' :
+             $type =~ /serial|int(eger)?/i ? '4' :
+             $type =~ /small(int|serial)/i ? '2' :
+             '0',
+         #rational
+            '0',
+         #time
+            '0',
+         #defautlValue
+            ""
+    );
+       
+       return '' if $_type eq "error";
+	 
+	 if($_type eq "float") {
+		$rational = "true";
+        $_type    = "number";
+	 }
+     elsif($_type eq "integer") {
+        $rational = "false";
+        $_type    = "number";
+     }
+     elsif($_type eq "date") {
+        $time = $type eq "date" ? "false" : "true";
+     }
+     
+     if( $options =~ /default\s+?(.+?)$/i ) {
+        my $s = $1;
+        $s =~ s/'//g;
+        $defaultValue = ", defaultValue: \"". ((substr $s, -1, 1) eq ',' ? (substr $s, 0 , -1)  : $s )   ."\"";
+    }
+
+    return
+	"{ type: '$_type', required: " .
+	(
+		$options =~ /not null/i
+			? "true"
+			: "false"
+	)
+    .
+    (
+		$type_size ne '0'
+			? ", size: $type_size"
+			: ''
+	)
+    .
+    (
+        $rational  ne '0'
+            ? ", rational: $rational"
+            : ''
+    )
+    .
+    (
+        $time ne '0'
+        ? ", time: $time "
+        : ''
+    )
+    .
+    $defaultValue
+    .
+    "}";
+}
+
+sub get_options {
+    our( @ret ) = ("", "","");
+    if ( $_[0] =~ /"?(?<name>[a-z0-9_]+)"?\s+?(?<type>[[a-z0-9\(\)_]+)\s*(?<options>.+?)?$/i ) {
+        $ret[0] = "$+{name}";
+        $ret[1] = "$+{type}";
+        $ret[2] = "$+{options}" || "";
+    }
+	die("unable to extract name or type from: $_[0]: $ret[0] $ret[1]") if $ret[0] eq "" or $ret[1] eq "";
+    return @ret;
+}
